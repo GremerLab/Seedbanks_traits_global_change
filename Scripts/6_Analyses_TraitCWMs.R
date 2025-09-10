688b353a89a68f502f752f2939c48462689932b6
jrgremer <jrgremer@ucdavis.edu>
2025-09-10 13:29:47 -0700

    CWM models and figs done, focus on individual traits

--- Scripts/6_Analyses_TraitCWMs.R
+++ Scripts/6_Analyses_TraitCWMs.R
@@ -13,6 +13,7 @@ library(modelsummary)
 library(kableExtra) #tables of model output
 library(emmeans) #post hoc contrasts
 library(ggplot2)
+library(cowplot) #for arranging panels in figures
 
 traitdat = read.csv("Output data/Traits_PCscores_all.csv") #see 5_Analyses_TraitPCA.R for script generating this file
 #note traitdat is using the transformed trait values as they were input into the PCA in script 5
@@ -275,41 +276,249 @@ cld_SCT = as.data.frame(cld(SCT_emm,
           
 ## Mass ##
 #2 way: hab x fert is sig #
-m1=lme((responsevar)~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=modeldat,na.action=na.exclude,method = "REML")
+Mass_lm=lme(Mass_CWM ~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=cwmdat,na.action=na.exclude,method = "REML")
+anova(mass_lm)
+
+Mass_emm = emmeans(Mass_lm,  ~ habitat*watering*fertilization) #keep all factors in here to generate letters
+
+cld_Mass = as.data.frame(cld(Mass_emm, 
+                            adjust = "Tukey",     # p-value adjustment
+                            Letters = letters,    # Specify letters to use
+                            alpha = 0.05,         # Significance level
+                            reversed = TRUE) )  %>%    # Sort means in decreasing order
+  rename(Mass_letters = .group)  %>% # Rename the letters column
+  mutate(WFtreatment = as.factor(case_when(
+    watering == "none" & fertilization == "none" ~ "C",
+    watering == "watered" & fertilization == "none" ~ "R",
+    watering == "none" & fertilization == "fertilized" ~ "N",
+    watering == "watered" & fertilization == "fertilized" ~ "NR"
+  ))) %>%
+  mutate(WFtreatment_order = factor(WFtreatment, levels = c("C","R", "N", "NR")))
 
 ## SCP ##
 #2 way: hab x watering is sig #
-m1=lme((responsevar)~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=modeldat,na.action=na.exclude,method = "REML")
+SCP_lm=lme(SCP_CWM ~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=cwmdat,na.action=na.exclude,method = "REML")
+
+anova(SCP_lm) 
+
+SCP_emm = emmeans(SCP_lm,  ~ habitat*watering*fertilization) 
+
+cld_SCP = as.data.frame(cld(SCP_emm, 
+                            adjust = "Tukey",     # p-value adjustment
+                            Letters = letters,    # Specify letters to use
+                            alpha = 0.05,         # Significance level
+                            reversed = TRUE) )  %>%    # Sort means in decreasing order
+  rename(SCP_letters = .group)  %>% # Rename the letters column
+  mutate(WFtreatment = as.factor(case_when(
+    watering == "none" & fertilization == "none" ~ "C",
+    watering == "watered" & fertilization == "none" ~ "R",
+    watering == "none" & fertilization == "fertilized" ~ "N",
+    watering == "watered" & fertilization == "fertilized" ~ "NR"
+  ))) %>%
+  mutate(WFtreatment_order = factor(WFtreatment, levels = c("C","R", "N", "NR")))
 
 ## CN ##
 #Only main effects of habitat and fertilization are sig #
-m2=lme((responsevar)~ habitat+ watering+ fertilization,random=~1|Line, data=modeldat,na.action=na.exclude,method = "REML")
+CN_lm =lme(CN_CWM~ habitat+ watering+ fertilization,random=~1|Line, data=cwmdat,na.action=na.exclude,method = "REML")
+anova(CN_lm) 
+
+CN_emm = emmeans(CN_lm,  ~ habitat*watering*fertilization) 
+
+cld_CN = as.data.frame(cld(CN_emm, 
+                               adjust = "Tukey",     # p-value adjustment
+                               Letters = letters,    # Specify letters to use
+                               alpha = 0.05,         # Significance level
+                               reversed = TRUE) )  %>%    # Sort means in decreasing order
+  rename(CN_letters = .group)  %>% # Rename the letters column
+  mutate(WFtreatment = as.factor(case_when(
+    watering == "none" & fertilization == "none" ~ "C",
+    watering == "watered" & fertilization == "none" ~ "R",
+    watering == "none" & fertilization == "fertilized" ~ "N",
+    watering == "watered" & fertilization == "fertilized" ~ "NR"
+  ))) %>%
+  mutate(WFtreatment_order = factor(WFtreatment, levels = c("C","R", "N", "NR")))
 
 ## Length ## 
 #2 way: hab x fert is sig #
-m1=lme((responsevar)~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=modeldat,na.action=na.exclude,method = "REML")
+Length_lm=lme(Length_CWM~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=cwmdat,na.action=na.exclude,method = "REML")
+
+anova(Length_lm) 
+
+Length_emm = emmeans(Length_lm,  ~ habitat*watering*fertilization) 
+
+cld_Length = as.data.frame(cld(Length_emm, 
+                            adjust = "Tukey",     # p-value adjustment
+                            Letters = letters,    # Specify letters to use
+                            alpha = 0.05,         # Significance level
+                            reversed = TRUE) )  %>%    # Sort means in decreasing order
+  rename(Length_letters = .group)  %>% # Rename the letters column
+  mutate(WFtreatment = as.factor(case_when(
+    watering == "none" & fertilization == "none" ~ "C",
+    watering == "watered" & fertilization == "none" ~ "R",
+    watering == "none" & fertilization == "fertilized" ~ "N",
+    watering == "watered" & fertilization == "fertilized" ~ "NR"
+  ))) %>%
+  mutate(WFtreatment_order = factor(WFtreatment, levels = c("C","R", "N", "NR")))
 
 ## Starch ## 
 #2 ways: hab x fert and hab x watering are sig #
-m1=lme((responsevar)~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=modeldat,na.action=na.exclude,method = "REML")
+Starch_lm =lme(Starch_CWM ~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=cwmdat,na.action=na.exclude,method = "REML")
+anova(Starch_lm) 
 
+Starch_emm = emmeans(Starch_lm,  ~ habitat*watering*fertilization) 
+
+cld_Starch = as.data.frame(cld(Starch_emm, 
+                            adjust = "Tukey",     # p-value adjustment
+                            Letters = letters,    # Specify letters to use
+                            alpha = 0.05,         # Significance level
+                            reversed = TRUE) )  %>%    # Sort means in decreasing order
+  rename(Starch_letters = .group)  %>% # Rename the letters column
+  mutate(WFtreatment = as.factor(case_when(
+    watering == "none" & fertilization == "none" ~ "C",
+    watering == "watered" & fertilization == "none" ~ "R",
+    watering == "none" & fertilization == "fertilized" ~ "N",
+    watering == "watered" & fertilization == "fertilized" ~ "NR"
+  ))) %>%
+  mutate(WFtreatment_order = factor(WFtreatment, levels = c("C","R", "N", "NR")))
 ## Shape ##
-#habitat x fert is sig #
-m1=lme((responsevar)~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=modeldat,na.action=na.exclude,method = "REML")
+#habitat x fert is sig, habitat x watering is marginally sig#
+Shape_lm =lme(Shape_CWM ~ habitat*watering+habitat*fertilization+watering*fertilization,random=~1|Line, data=cwmdat,na.action=na.exclude,method = "REML")
 
+anova(Shape_lm) 
+
+Shape_emm = emmeans(Shape_lm,  ~ habitat*watering*fertilization) 
+
+cld_Shape = as.data.frame(cld(Shape_emm, 
+                            adjust = "Tukey",     # p-value adjustment
+                            Letters = letters,    # Specify letters to use
+                            alpha = 0.05,         # Significance level
+                            reversed = TRUE) )  %>%    # Sort means in decreasing order
+  rename(Shape_letters = .group)  %>% # Rename the letters column
+  mutate(WFtreatment = as.factor(case_when(
+    watering == "none" & fertilization == "none" ~ "C",
+    watering == "watered" & fertilization == "none" ~ "R",
+    watering == "none" & fertilization == "fertilized" ~ "N",
+    watering == "watered" & fertilization == "fertilized" ~ "NR"
+  ))) %>%
+  mutate(WFtreatment_order = factor(WFtreatment, levels = c("C","R", "N", "NR")))
 
 
 #### Figures ####
-sctplot = ggplot(data=cwmdat,aes(x=WFtreatment_order, y=SCT_CWM, fill = habitat))+
+SCTplot = ggplot(data=cwmdat,aes(x=WFtreatment_order, y=SCT_CWM, fill = habitat))+
   geom_boxplot(position = position_dodge(.9))+
-  labs(title="",subtitle = "",y="Community weighted mean SCT", x="")+
+  labs(title="",subtitle = "",y="CWM SCT", x="")+
   scale_fill_grey(start=0.9, end=0.4) +
   scale_color_grey(start=0.4, end=0.7) +
   #facet_grid(rows = vars(type), cols=vars(habitat),switch="y",scales = "free")+
   theme_bw()+
   theme(panel.spacing = unit(0, units = "cm"), 
-        legend.position = "bottom",legend.title = element_blank()) + labs(fill = "Habitat")
+        legend.position = "bottom",legend.title = element_blank(),axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))  +
+         labs(fill = "Habitat")
 
-sctplot + geom_text(data = cld_SCT, aes( x = WFtreatment_order, y = 2, group = habitat, label = SCT_letters ),
+a= SCTplot + geom_text(data = cld_SCT, aes( x = WFtreatment_order, y = 2.1, group = habitat, label = SCT_letters ),
+                    position = position_dodge(width = 0.9))
+a
+
+Massplot = ggplot(data=cwmdat,aes(x=WFtreatment_order, y=Mass_CWM, fill = habitat))+
+  geom_boxplot(position = position_dodge(.9))+
+  labs(title="",subtitle = "",y="CWM Mass", x="")+
+  scale_fill_grey(start=0.9, end=0.4) +
+  scale_color_grey(start=0.4, end=0.7) +
+  #facet_grid(rows = vars(type), cols=vars(habitat),switch="y",scales = "free")+
+  theme_bw()+
+  theme(panel.spacing = unit(0, units = "cm"), 
+        legend.position = "bottom",legend.title = element_blank(),axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))  +
+  labs(fill = "Habitat")
+
+b= Massplot + geom_text(data = cld_Mass, aes( x = WFtreatment_order, y = 2, group = habitat, label = Mass_letters ),
                     position = position_dodge(width = 0.9))
 
+
+SCPplot = ggplot(data=cwmdat,aes(x=WFtreatment_order, y=SCP_CWM, fill = habitat))+
+  geom_boxplot(position = position_dodge(.9))+
+  labs(title="",subtitle = "",y="CWM SCP", x="")+
+  scale_fill_grey(start=0.9, end=0.4) +
+  scale_color_grey(start=0.4, end=0.7) +
+  #facet_grid(rows = vars(type), cols=vars(habitat),switch="y",scales = "free")+
+  theme_bw()+
+  theme(panel.spacing = unit(0, units = "cm"), 
+        legend.position = "bottom",legend.title = element_blank(),axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))  +
+  labs(fill = "Habitat")
+
+c= SCPplot + geom_text(data = cld_SCP, aes( x = WFtreatment_order, y = 2.1, group = habitat, label = SCP_letters ),
+                    position = position_dodge(width = 0.9))
+
+CNplot = ggplot(data=cwmdat,aes(x=WFtreatment_order, y=CN_CWM, fill = habitat))+
+  geom_boxplot(position = position_dodge(.9))+
+  labs(title="",subtitle = "",y="CWM CN", x="")+
+  scale_fill_grey(start=0.9, end=0.4) +
+  scale_color_grey(start=0.4, end=0.7) +
+  #facet_grid(rows = vars(type), cols=vars(habitat),switch="y",scales = "free")+
+  theme_bw()+
+  theme(panel.spacing = unit(0, units = "cm"), 
+        legend.position = "bottom",legend.title = element_blank(),axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))  +
+  labs(fill = "Habitat")
+
+d= CNplot + geom_text(data = cld_CN, aes( x = WFtreatment_order, y = 26, group = habitat, label = CN_letters ),
+                    position = position_dodge(width = 0.9))
+Lengthplot = ggplot(data=cwmdat,aes(x=WFtreatment_order, y=Length_CWM, fill = habitat))+
+  geom_boxplot(position = position_dodge(.9))+
+  labs(title="",subtitle = "",y="CWM Length", x="")+
+  scale_fill_grey(start=0.9, end=0.4) +
+  scale_color_grey(start=0.4, end=0.7) +
+  #facet_grid(rows = vars(type), cols=vars(habitat),switch="y",scales = "free")+
+  theme_bw()+
+  theme(panel.spacing = unit(0, units = "cm"), 
+        legend.position = "bottom",legend.title = element_blank(),axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))  +
+  labs(fill = "Habitat")
+
+e =Lengthplot + geom_text(data = cld_Length, aes( x = WFtreatment_order, y = 2.5, group = habitat, label = Length_letters ),
+                    position = position_dodge(width = 0.9))
+Starchplot = ggplot(data=cwmdat,aes(x=WFtreatment_order, y=Starch_CWM, fill = habitat))+
+  geom_boxplot(position = position_dodge(.9))+
+  labs(title="",subtitle = "",y="CWM Starch", x="")+
+  scale_fill_grey(start=0.9, end=0.4) +
+  scale_color_grey(start=0.4, end=0.7) +
+  #facet_grid(rows = vars(type), cols=vars(habitat),switch="y",scales = "free")+
+  theme_bw()+
+  theme(panel.spacing = unit(0, units = "cm"), 
+        legend.position = "bottom",legend.title = element_blank(),axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))  +
+  labs(fill = "Habitat")
+
+f= Starchplot + geom_text(data = cld_Starch, aes( x = WFtreatment_order, y = 1.05, group = habitat, label = Starch_letters ),
+                    position = position_dodge(width = 0.9))
+
+
+Shapeplot = ggplot(data=cwmdat,aes(x=WFtreatment_order, y=Shape_CWM, fill = habitat))+
+  geom_boxplot(position = position_dodge(.9))+
+  labs(title="",subtitle = "",y="CWM Shape", x="")+
+  scale_fill_grey(start=0.9, end=0.4) +
+  scale_color_grey(start=0.4, end=0.7) +
+  #facet_grid(rows = vars(type), cols=vars(habitat),switch="y",scales = "free")+
+  theme_bw()+
+  theme(panel.spacing = unit(0, units = "cm"), 
+        legend.position = "bottom",legend.title = element_blank(),axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))  +
+  labs(fill = "Habitat")
+
+g= Shapeplot + geom_text(data = cld_Shape, aes( x = WFtreatment_order, y = -1.65, group = habitat, label = Shape_letters ),
+                    position = position_dodge(width = 0.9))
+### save figure 2: CWM traits ###
+plot_grid(a + theme(legend.position = "none"),
+          b+ theme(legend.position = "none"),
+          c+ theme(legend.position = "none"),
+          d+ theme(legend.position = "none"),
+         # e+ theme(legend.position = "none"), #ECE didn't have length in her graph, could add/remove here
+          f, 
+          g,
+          ncol = 2, nrow = 3,
+          labels = c("A.", "B.", "C.", "D.", "E.", "F.", "G."), label_size=14)
+#ggsave("Plots/Fig2_CWMresponses.jpg", height = 10, width = 15)
+e
+#ggsave("Plots/Fig2_CWMresponses_length.jpg", height = 5, width = 8)
